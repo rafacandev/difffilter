82ffe0c7ef006977835a023135d540586e855984
Rafael Santos <rafael.santos.bra@gmail.com>
2017-02-20 11:33:57 -0500

    Added AppClientHandlerIT.java

---
+++ src/test/java/com/github/rafasantos/cli/AppCliHandlerIT.java
@@ -0,0 +1,27 @@
+package com.github.rafasantos.cli;
+
+import java.io.FileNotFoundException;
+import java.net.URL;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.github.rafasantos.cli.AppCliHandler;
+
+public class AppCliHandlerIT {
+	
+	@Test(expected=FileNotFoundException.class)
+	public void fileDoesNotExist_ff() throws FileNotFoundException {
+		URL secondFile = this.getClass().getClassLoader().getResource("second-file.tsv");
+		String[] arguments = {"-ff", "files/does/not/exist/path", "-sf", secondFile.getPath()};
+		new AppCliHandler(arguments);
+	}
+	
+	@Test(expected=FileNotFoundException.class)
+	public void fileDoesNotExist_sf() throws FileNotFoundException {
+		URL firstFile = this.getClass().getClassLoader().getResource("first-file.tsv");
+		String[] arguments = {"-ff", firstFile.getPath(), "-sf", "files/does/not/exist/path"};
+		new AppCliHandler(arguments);
+	}
+	
+}
